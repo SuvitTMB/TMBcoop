e84d5bd16d787aaf4827992db00bdbfd13af513c
SuvitTMB <82767838+SuvitTMB@users.noreply.github.com>
2024-12-30 19:10:26 +0700

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -64,6 +64,23 @@ async function getUserProfile() {
 }
 
 
+function Connect_DB() {
+  var firebaseConfig = {
+    apiKey: "AIzaSyAciknEYhZU7AwOdfYytC1t_AnW2Ee11us",
+    authDomain: "faifah-ttb.firebaseapp.com",
+    databaseURL: "https://file-upload-faifah-ttb.firebaseio.com",
+    projectId: "faifah-ttb",
+    storageBucket: "faifah-ttb.appspot.com",
+    messagingSenderId: "842980876200",
+    appId: "1:842980876200:web:f33bfad2ccbf263075079d",
+    measurementId: "G-DFMSBDT6W8"
+  };
+  firebase.initializeApp(firebaseConfig);
+  dbTMBcoopMember = firebase.firestore().collection("TMBcoop_User");
+  dbTMBcoopLog = firebase.firestore().collection("TMBcoop_Log");
+}
+
+
 var xCheckUser = "";
 function CheckEmpID() {
   NewDate();
